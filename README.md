ccc71a18280e5c7f9b17b880fc22cd9d8f28d5fa
Gabriel Sanchez <gabrielsz87@gmail.com>
2024-03-18 11:46:24 -0500

    add prompt template lesson code

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # starter-lanchainjs
+
+## Installation
+
+1. Fork / clone this repository.
+1. Run `npm install`.
+
+Use `npm start` to run the application.
--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 import { ChatCohere } from "@langchain/cohere";
+import { PromptTemplate } from "@langchain/core/prompts";
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const model = new ChatCohere({apiKey: process.env.COHERE_API_KEY});
+
+const promptTemplate = PromptTemplate.fromTemplate(
+  "Give me a recipe for {food}"
+);
+
+const chain = promptTemplate.pipe(model);
+
+const result = await chain.invoke({ food: "apple pie" });
+
+console.log(result);
