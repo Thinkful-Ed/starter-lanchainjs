7148b60a658caffa473bc55b0cf1a466c646a853
Gabriel Sanchez <gabrielsz87@gmail.com>
2024-03-18 12:07:13 -0500

    add chain code

--- index.js
+++ index.js
@@ -2,22 +2,34 @@ import { ChatCohere } from "@langchain/cohere";
 import { PromptTemplate } from "@langchain/core/prompts";
 import { RunnableSequence } from "@langchain/core/runnables";
 import { StringOutputParser } from "@langchain/core/output_parsers";
-
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const model = new ChatCohere({apiKey: process.env.COHERE_API_KEY});
 
-const promptTemplate = PromptTemplate.fromTemplate(
-  "Give me a recipe for {food}"
+const prompt1 = PromptTemplate.fromTemplate(
+  `You are a master chef. Give me a recipe for {food}.`
+);
+const prompt2 = PromptTemplate.fromTemplate(
+  `You are a translator. Translate the recipe to {language}.`
 );
 
-// const chain = promptTemplate.pipe(model);
+const chain = prompt1.pipe(model).pipe(new StringOutputParser());
 
-const outputParser = new StringOutputParser();
-const chain = RunnableSequence.from([promptTemplate, model, outputParser]);
+const combinedChain = RunnableSequence.from([
+  {
+    food: chain,
+    language: (input) => input.language,
+  },
+  prompt2,
+  model,
+  new StringOutputParser(),
+]);
 
-const result = await chain.invoke({ food: "apple pie" });
+const result = await combinedChain.invoke({
+  food: "Blueberry muffins",
+  language: "Spanish",
+});
 
 console.log(result);
