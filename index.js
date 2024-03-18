fd31ea4f3e13dfc1d182c5e8820d3247b05082c3
Gabriel Sanchez <gabrielsz87@gmail.com>
2024-03-18 11:51:39 -0500

    add output parser

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import { ChatCohere } from "@langchain/cohere";
 import { PromptTemplate } from "@langchain/core/prompts";
+import { RunnableSequence } from "@langchain/core/runnables";
+import { StringOutputParser } from "@langchain/core/output_parsers";
+
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -10,7 +13,10 @@ const promptTemplate = PromptTemplate.fromTemplate(
   "Give me a recipe for {food}"
 );
 
-const chain = promptTemplate.pipe(model);
+// const chain = promptTemplate.pipe(model);
+
+const outputParser = new StringOutputParser();
+const chain = RunnableSequence.from([promptTemplate, model, outputParser]);
 
 const result = await chain.invoke({ food: "apple pie" });
 
